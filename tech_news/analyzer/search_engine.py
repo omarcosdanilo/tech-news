29b323096818dfd8356387c6a54d7592a1021d3f
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-10 20:47:29 -0300

    feat: req7 - cria a função search_by_date
    a função recebe uma string no formato ISO, faz uma busca no db a partir desta data e retorna os resultados

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,24 @@
 from tech_news.database import search_news
+from datetime import date as date_obj
 
 
 # Requisito 6
 def search_by_title(title):
-    news = search_news({'title': {'$regex': f'{title}', '$options': 'i'}})
+    news = search_news({"title": {"$regex": f"{title}", "$options": "i"}})
 
-    return [
-        (content['title'], content['url'])
-        for content in news
-        ]
+    return [(content["title"], content["url"]) for content in news]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formated_date = date_obj.fromisoformat(date).strftime("%d/%m/%Y")
+
+        news = search_news({"timestamp": formated_date})
+
+        return [(content["title"], content["url"]) for content in news]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
