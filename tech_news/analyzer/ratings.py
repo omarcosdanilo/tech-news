fc80b1faec7c21eeb6b8820a06a96fa0ed671937
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-11 15:25:24 -0300

    feat: :sparkles: req11 - cria a função top_5_categories
    a função retorna uma lista com as 5 categorias com maior ocorrência no db

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -16,4 +16,18 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    news_categories = {}
+
+    for item in news:
+        if item["category"] not in news_categories:
+            news_categories[item["category"]] = 1
+        else:
+            news_categories[item["category"]] += 1
+
+    top_categories = sorted(
+        news_categories.items(), key=lambda x: (-x[1], x[0])
+    )
+
+    return [category[0] for category in top_categories]
