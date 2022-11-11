551866659a0d67610d07ae68fa45b2e9f706079c
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-11 14:50:08 -0300

    feat: :sparkles: req10 - cria a função top_5_news
    a função ordena as noticias do db por ordem decrescente de comentários e retorna as 5 primeiras caso tenha mais de 5 items no db

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+
+
 # Requisito 10.
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    sorted_by_comments_count = sorted(
+        news, key=lambda x: x["comments_count"], reverse=True
+    )
+
+    return [
+        (content["title"], content["url"])
+        for content in sorted_by_comments_count
+    ][:5]
 
 
 # Requisito 11
